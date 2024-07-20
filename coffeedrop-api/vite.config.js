7c7bacd6eba95326dd0e129079392f43447b4cf4
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-07-20 18:04:16 +0100

    Make Vite use env vars for host and port

--- coffeedrop-api/vite.config.js
+++ coffeedrop-api/vite.config.js
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig, loadEnv } from "vite";
+import laravel from "laravel-vite-plugin";
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), "");
+	return {
+		plugins: [
+			laravel({
+				input: ["resources/css/app.css", "resources/js/app.js"],
+				refresh: true,
+			}),
+		],
+		server: {
+			hmr: {
+				host: "localhost",
+			},
+			host: env.VITE_SERVER_HOST,
+			port: env.VITE_SERVER_PORT,
+		},
+	};
 });
