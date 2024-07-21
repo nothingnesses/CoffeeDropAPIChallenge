fa586d5ef3c20d32c75aee2d6447429461b7c484
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-07-21 23:05:17 +0100

    Add "/get-nearest-location/{postcode}" GET route

--- coffeedrop-api/routes/api.php
+++ coffeedrop-api/routes/api.php
@@ -1,8 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\BusinessHoursController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/get-nearest-location/{postcode}', [BusinessHoursController::class, 'get_nearest_location']);
