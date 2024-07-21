e49ffa3cf4e5c71084881020b0bdda24e373bbf7
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-07-21 13:55:31 +0100

    Implement `add_columns_to_locations_table` migration

--- coffeedrop-api/database/migrations/2024_07_21_125224_add_columns_to_locations_table.php
+++ coffeedrop-api/database/migrations/2024_07_21_125224_add_columns_to_locations_table.php
@@ -1,28 +1,52 @@
 <?php
 
+use App\Models\PostcodeDayMapping;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::table('locations', function (Blueprint $table) {
-            //
-        });
-    }
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void {
+		Schema::table('locations', function (Blueprint $table) {
+			$table
+				->foreignIdFor(PostcodeDayMapping::class)
+				->unique()
+				->nullable(false)
+				->constrained()
+				->cascadeOnUpdate()
+				->cascadeOnDelete();
+			$table
+				->unsignedBigInteger('opening_time_id')
+				->nullable();
+			$table
+				->foreign('opening_time_id')
+				->references('id')
+				->on('times')
+				->cascadeOnUpdate()
+				->cascadeOnDelete();
+			$table
+				->unsignedBigInteger('closing_time_id')
+				->nullable();
+			$table
+				->foreign('closing_time_id')
+				->references('id')
+				->on('times')
+				->cascadeOnUpdate()
+				->cascadeOnDelete();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('locations', function (Blueprint $table) {
-            //
-        });
-    }
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void {
+		Schema::table('locations', function (Blueprint $table) {
+			$table->dropColumn('closing_time_id');
+			$table->dropColumn('opening_time_id');
+			$table->dropConstrainedForeignIdFor(PostcodeDayMapping::class);
+		});
+	}
 };
