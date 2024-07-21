cdd49004c8a13af3dcead1875c395c74e95bbaf8
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-07-21 12:50:58 +0100

    Implement the `add_day_column_to_days_table` migration

--- coffeedrop-api/database/migrations/2024_07_21_114647_add_day_column_to_days_table.php
+++ coffeedrop-api/database/migrations/2024_07_21_114647_add_day_column_to_days_table.php
@@ -4,25 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::table('days', function (Blueprint $table) {
-            //
-        });
-    }
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void {
+		Schema::table('days', function (Blueprint $table) {
+			$table
+				->string('day', 9)
+				->unique()
+				->nullable(false);
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('days', function (Blueprint $table) {
-            //
-        });
-    }
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void {
+		Schema::table('days', function (Blueprint $table) {
+			$table->dropColumn('day');
+		});
+	}
 };
