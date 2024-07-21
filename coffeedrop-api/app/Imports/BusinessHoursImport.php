60bcafef73d4b351d89a82e5c418dc5fee8711ba
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-07-21 19:07:53 +0100

    Implement `BusinessHoursImport`

--- coffeedrop-api/app/Imports/BusinessHoursImport.php
+++ coffeedrop-api/app/Imports/BusinessHoursImport.php
@@ -3,19 +3,68 @@
 namespace App\Imports;
 
 use App\Models\BusinessHours;
-use Maatwebsite\Excel\Concerns\ToModel;
+use App\Models\Day;
+use App\Models\Postcode;
+use App\Models\Time;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\SkipsErrors;
+use Maatwebsite\Excel\Concerns\SkipsOnError;
+use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class BusinessHoursImport implements ToModel
-{
-    /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
-    public function model(array $row)
-    {
-        return new BusinessHours([
-            //
-        ]);
-    }
+class BusinessHoursImport implements ToCollection, WithHeadingRow, SkipsOnError {
+	use SkipsErrors;
+
+	/**
+	 * @param \Illuminate\Support\Collection $rows
+	 */
+	public function collection(Collection $rows) {
+		$get_day_id = fn($day) => Day::query()
+			->where('day', $day)
+			->get()
+			->first()
+			->id;
+		$get_time_id = fn($time) => empty($time)
+			? NULL
+			: Time::query()
+				->where('time', $time)
+				->get()
+				->first()
+				->id;
+		$days = collect([
+			'Monday',
+			'Tuesday',
+			'Wednesday',
+			'Thursday',
+			'Friday',
+			'Saturday',
+			'Sunday'
+		])
+			->mapWithKeys(
+				fn($item, $key) => [
+					$item => [
+						'day_id' => $get_day_id($item),
+						'opening_time_key' => 'open_' . strtolower($item),
+						'closing_time_key' => 'closed_' . strtolower($item)
+					]
+				]
+			);
+		foreach ($rows as $row) {
+			if (!empty($row['postcode'])) {
+				$postcode_id = Postcode::query()
+					->where('postcode', $row['postcode'])
+					->get()
+					->first()
+					->id;
+				foreach ($days as $key => $value) {
+					BusinessHours::firstOrCreate([
+						'postcode_id' => $postcode_id,
+						'day_id' => $value['day_id'],
+						'opening_time_id' => $get_time_id($row[$value['opening_time_key']]),
+						'closing_time_id' => $get_time_id($row[$value['closing_time_key']]),
+					]);
+				}
+			}
+		}
+	}
 }
