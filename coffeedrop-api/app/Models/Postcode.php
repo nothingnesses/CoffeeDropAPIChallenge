f6e1d68c08ad0eb91e11aa4ce44fe15e0c735daf
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-07-21 23:01:58 +0100

    Add has-many `business_hours` relationship to `Postcode`

--- coffeedrop-api/app/Models/Postcode.php
+++ coffeedrop-api/app/Models/Postcode.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Postcode extends Model
 {
     use HasFactory;
+
+    public function business_hours(): HasMany {
+        return $this->hasMany(BusinessHours::class);
+    }
 }
