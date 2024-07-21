3d8b07f95297d04de29118fb0bd22883b8c89c9b
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-07-21 14:31:50 +0100

    Implement `DaySeeder`

--- coffeedrop-api/database/seeders/DaySeeder.php
+++ coffeedrop-api/database/seeders/DaySeeder.php
@@ -4,14 +4,19 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
-class DaySeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        //
-    }
+class DaySeeder extends Seeder {
+	/**
+	 * Run the database seeds.
+	 */
+	public function run(): void {
+		DB::table('days')->updateOrInsert(['day' => 'Monday']);
+		DB::table('days')->updateOrInsert(['day' => 'Tuesday']);
+		DB::table('days')->updateOrInsert(['day' => 'Wednesday']);
+		DB::table('days')->updateOrInsert(['day' => 'Thursday']);
+		DB::table('days')->updateOrInsert(['day' => 'Friday']);
+		DB::table('days')->updateOrInsert(['day' => 'Saturday']);
+		DB::table('days')->updateOrInsert(['day' => 'Sunday']);
+	}
 }
