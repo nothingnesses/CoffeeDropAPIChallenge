a70d58fb51fd074b56d947595b72436a04ebaa46
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-07-21 19:11:51 +0100

    Implement `BusinessHoursSeeder`

--- coffeedrop-api/database/seeders/BusinessHoursSeeder.php
+++ coffeedrop-api/database/seeders/BusinessHoursSeeder.php
@@ -2,16 +2,16 @@
 
 namespace Database\Seeders;
 
+use App\Imports\BusinessHoursImport;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Maatwebsite\Excel\Facades\Excel;
 
-class BusinessHoursSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        //
-    }
+class BusinessHoursSeeder extends Seeder {
+	/**
+	 * Run the database seeds.
+	 */
+	public function run(): void {
+		Excel::import(new BusinessHoursImport, public_path('location_data.csv'));
+	}
 }
