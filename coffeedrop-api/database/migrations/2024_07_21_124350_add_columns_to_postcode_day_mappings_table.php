5c0fad7f47835e318284427723786e36eee38da0
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-07-21 13:45:21 +0100

    Implement `add_columns_to_postcode_day_mappings_table` migration

--- coffeedrop-api/database/migrations/2024_07_21_124350_add_columns_to_postcode_day_mappings_table.php
+++ coffeedrop-api/database/migrations/2024_07_21_124350_add_columns_to_postcode_day_mappings_table.php
@@ -1,28 +1,43 @@
 <?php
 
+use App\Models\Day;
+use App\Models\Postcode;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::table('postcode_day_mappings', function (Blueprint $table) {
-            //
-        });
-    }
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void {
+		Schema::table('postcode_day_mappings', function (Blueprint $table) {
+			$table
+				->foreignIdFor(Postcode::class, 'postcode_id')
+				->nullable(false)
+				->constrained()
+				->cascadeOnUpdate()
+				->cascadeOnDelete();
+			$table
+				->foreignIdFor(Day::class, 'day_id')
+				->nullable(false)
+				->constrained()
+				->cascadeOnUpdate()
+				->cascadeOnDelete();
+			$table->unique([
+				'postcode_id',
+				'day_id'
+			]);
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('postcode_day_mappings', function (Blueprint $table) {
-            //
-        });
-    }
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void {
+		Schema::table('postcode_day_mappings', function (Blueprint $table) {
+			$table->dropConstrainedForeignIdFor(Day::class);
+			$table->dropConstrainedForeignIdFor(Postcode::class);
+		});
+	}
 };
