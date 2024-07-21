f068f135b4c535213715d12b8d4257bbbcc80c9b
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-07-21 14:33:15 +0100

    Update `UserSeeder`

--- coffeedrop-api/database/seeders/UserSeeder.php
+++ coffeedrop-api/database/seeders/UserSeeder.php
@@ -11,11 +11,11 @@ class UserSeeder extends Seeder {
 	 * Run the database seeds.
 	 */
 	public function run(): void {
-		// User::factory(10)->create();
+		User::factory(10)->create();
 
-		User::factory()->create([
-			'name' => 'Test User',
-			'email' => 'test@example.com',
-		]);
+		// User::factory()->create([
+		// 	'name' => 'Test User',
+		// 	'email' => 'test@example.com',
+		// ]);
 	}
 }
