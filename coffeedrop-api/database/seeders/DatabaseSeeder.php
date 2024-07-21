c3a37a1cc92c5c0be83d65b76a6c62a64bbfc009
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-07-21 12:19:49 +0100

    Add new seeders to `DatabaseSeeder`

--- coffeedrop-api/database/seeders/DatabaseSeeder.php
+++ coffeedrop-api/database/seeders/DatabaseSeeder.php
@@ -12,6 +12,10 @@ class DatabaseSeeder extends Seeder {
 	public function run(): void {
 		$this->call([
 			UserSeeder::class,
+			DaySeeder::class,
+			PostcodeSeeder::class,
+			TimeSeeder::class,
+			LocationSeeder::class,
 		]);
 	}
 }
