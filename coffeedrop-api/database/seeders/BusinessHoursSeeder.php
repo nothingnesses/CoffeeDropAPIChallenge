a5ba0780f0658d0d792f2888193fdc9b11b5a47e
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-07-21 12:17:17 +0100

    php artisan make:model BusinessHours -cmsr

---
+++ coffeedrop-api/database/seeders/BusinessHoursSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BusinessHoursSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
