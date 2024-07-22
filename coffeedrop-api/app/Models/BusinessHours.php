b029d2a5ce42385b0dcf94ac44c8cd1108a81d6c
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-07-22 10:07:25 +0100

    * Specify mass-assignable attributes for `BusinessHours`

--- coffeedrop-api/app/Models/BusinessHours.php
+++ coffeedrop-api/app/Models/BusinessHours.php
@@ -5,7 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class BusinessHours extends Model
-{
-    use HasFactory;
+class BusinessHours extends Model {
+	use HasFactory;
+
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array<int, string>
+	 */
+	protected $fillable = [
+		'postcode_id',
+		'day_id',
+		'opening_time_id',
+		'closing_time_id'
+	];
 }
