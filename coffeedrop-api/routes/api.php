882ca563468562790877c3a95f80009dfc488c72
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-07-22 10:09:17 +0100

    Add "/create-new-location" POST route

--- coffeedrop-api/routes/api.php
+++ coffeedrop-api/routes/api.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\BusinessHoursController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/get-nearest-location/{postcode}', [BusinessHoursController::class, 'get_nearest_location']);
+Route::post('/create-new-location', [BusinessHoursController::class, 'create_new_location']);
