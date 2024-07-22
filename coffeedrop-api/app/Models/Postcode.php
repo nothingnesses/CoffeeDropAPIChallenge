62b08a93811872e79ecc4e2d6113e3ed7727a8cb
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-07-22 09:15:57 +0100

    Update `Postcode` to automatically set coordinates when postcode is set

--- coffeedrop-api/app/Models/Postcode.php
+++ coffeedrop-api/app/Models/Postcode.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use JustSteveKing\LaravelPostcodes\Facades\Postcode as FacadesPostcode;
 
 class Postcode extends Model {
 	use HasFactory;
@@ -15,6 +17,17 @@ class Postcode extends Model {
 	 * @var array<int, string>
 	 */
 	protected $fillable = ['postcode'];
+
+	protected function postcode(): Attribute {
+		return Attribute::make(set: function ($value) {
+			$postcode_data = FacadesPostcode::getPostcode($value);
+			return [
+				'postcode' => $value,
+				'latitude' => $postcode_data->latitude,
+				'longitude' => $postcode_data->longitude,
+			];
+		});
+	}
 
 	public function business_hours(): HasMany {
 		return $this->hasMany(BusinessHours::class);
