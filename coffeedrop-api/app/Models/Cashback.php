bf126b398a77354f08fdb1c6c733121d8f5d2ae8
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-07-22 15:01:12 +0100

    Specify mass-assignable attributes for `Cashback`

--- coffeedrop-api/app/Models/Cashback.php
+++ coffeedrop-api/app/Models/Cashback.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Cashback extends Model
 {
     use HasFactory;
+
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array<int, string>
+	 */
+	protected $fillable = [
+		'ristretto',
+		'espresso',
+		'lungo',
+		'cashback_pounds',
+		'cashback_pence',
+	];
 }
