f9e8e99f3aa931d849bdcea4e409c73d69a34abe
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-07-22 15:02:41 +0100

    Add "/calculate-cashback" POST route

--- coffeedrop-api/routes/api.php
+++ coffeedrop-api/routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\BusinessHoursController;
+use App\Http\Controllers\CashbackController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/get-nearest-location/{postcode}', [BusinessHoursController::class, 'get_nearest_location']);
 Route::post('/create-new-location', [BusinessHoursController::class, 'create_new_location']);
+Route::post('/calculate-cashback', [CashbackController::class, 'calculate_cashback']);
