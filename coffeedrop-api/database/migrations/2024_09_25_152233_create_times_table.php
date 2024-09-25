f6d22dfb1015310f77b08e2adf870d671a1a697f
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-25 14:39:21 +0100

    Implement the `create_times_table` migration

--- coffeedrop-api/database/migrations/2024_09_25_152233_create_times_table.php
+++ coffeedrop-api/database/migrations/2024_09_25_152233_create_times_table.php
@@ -4,24 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('times', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void {
+		Schema::create('times', function (Blueprint $table) {
+			$table->id();
+			$table->timestamps();
+			$table
+				->time('time')
+				->unique()
+				->nullable(false);
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('times');
-    }
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void {
+		Schema::dropIfExists('times');
+	}
 };
