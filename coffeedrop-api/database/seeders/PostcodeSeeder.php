57144d38474da0282338ef539937123e95ff20f9
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-26 15:27:04 +0100

    Implement `PostcodeSeeder`

--- coffeedrop-api/database/seeders/PostcodeSeeder.php
+++ coffeedrop-api/database/seeders/PostcodeSeeder.php
@@ -2,16 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Imports\PostcodesImport;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Maatwebsite\Excel\Facades\Excel;
 
-class PostcodeSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        //
-    }
+class PostcodeSeeder extends Seeder {
+	use WithoutModelEvents;
+
+	/**
+	 * Run the database seeds.
+	 */
+	public function run(): void {
+		Excel::import(new PostcodesImport, public_path('location_data.csv'));
+	}
 }
