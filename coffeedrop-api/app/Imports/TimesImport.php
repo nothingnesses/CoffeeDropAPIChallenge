04fa4f7bcb3da5ae7b8c51375e15b9db9b8ee360
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-26 11:49:40 +0100

    Implement `TimesImport`

--- coffeedrop-api/app/Imports/TimesImport.php
+++ coffeedrop-api/app/Imports/TimesImport.php
@@ -3,19 +3,29 @@
 namespace App\Imports;
 
 use App\Models\Time;
-use Maatwebsite\Excel\Concerns\ToModel;
+use Carbon\Carbon;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\SkipsErrors;
+use Maatwebsite\Excel\Concerns\SkipsOnError;
+use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class TimesImport implements ToModel
-{
-    /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
-    public function model(array $row)
-    {
-        return new Time([
-            //
-        ]);
-    }
+class TimesImport implements ToCollection, WithHeadingRow, SkipsOnError {
+	use SkipsErrors;
+
+	/**
+	 * @param \Illuminate\Support\Collection $rows
+	 */
+	public function collection(Collection $rows) {
+		$headings = collect($rows[0] ?? [])
+			->keys()
+			->skip(1);
+		foreach ($rows as $row) {
+			foreach ($row as $key => $value) {
+				if (!empty($value) && $headings->contains($key)) {
+					Time::firstOrCreate(['time' => new Carbon($value)]);
+				}
+			}
+		}
+	}
 }
