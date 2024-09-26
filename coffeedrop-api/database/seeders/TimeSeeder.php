df2d7632d5c5165834b54d5b0bc3ae2b2aa818c7
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-26 12:10:11 +0100

    Implement `TimeSeeder`

--- coffeedrop-api/database/seeders/TimeSeeder.php
+++ coffeedrop-api/database/seeders/TimeSeeder.php
@@ -2,16 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Imports\TimesImport;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Maatwebsite\Excel\Facades\Excel;
 
-class TimeSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        //
-    }
+class TimeSeeder extends Seeder {
+	use WithoutModelEvents;
+
+	/**
+	 * Run the database seeds.
+	 */
+	public function run(): void {
+		Excel::import(new TimesImport, public_path('location_data.csv'));
+	}
 }
