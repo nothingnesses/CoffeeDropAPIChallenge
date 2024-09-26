2c18e41d0812babc2fb28cdeb6b5ddd8a99f55d2
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-26 18:20:20 +0100

    Implement `LocationSeeder`

--- coffeedrop-api/database/seeders/LocationSeeder.php
+++ coffeedrop-api/database/seeders/LocationSeeder.php
@@ -2,16 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Imports\LocationsImport;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Maatwebsite\Excel\Facades\Excel;
 
-class LocationSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        //
-    }
+class LocationSeeder extends Seeder {
+	use WithoutModelEvents;
+
+	/**
+	 * Run the database seeds.
+	 */
+	public function run(): void {
+		Excel::import(new LocationsImport, public_path('location_data.csv'));
+	}
 }
