70534f51133bb84e162465380bf0f0982a578f22
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-26 15:24:59 +0100

    Implement `PostcodesImport`

--- coffeedrop-api/app/Imports/PostcodesImport.php
+++ coffeedrop-api/app/Imports/PostcodesImport.php
@@ -3,19 +3,25 @@
 namespace App\Imports;
 
 use App\Models\Postcode;
+use Maatwebsite\Excel\Concerns\SkipsErrors;
+use Maatwebsite\Excel\Concerns\SkipsOnError;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class PostcodesImport implements ToModel
-{
-    /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
-    public function model(array $row)
-    {
-        return new Postcode([
-            //
-        ]);
-    }
+class PostcodesImport implements ToModel, WithHeadingRow, SkipsOnError {
+	use SkipsErrors;
+
+	/**
+	 * @param array $row
+	 *
+	 * @return \Illuminate\Database\Eloquent\Model|null
+	 */
+	public function model(array $row) {
+		return optional(
+			$row['postcode'] ?? null,
+			fn($postcode) => Postcode::firstOrCreate([
+				'postcode' => preg_replace('/\s+/', '', strtoupper($postcode)),
+			])
+		);
+	}
 }
