79b1b4903f919d53c8b778e98fedff83fb190d29
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-26 17:02:38 +0100

    Define mass assignable attributes of `Location`

--- coffeedrop-api/app/Models/Location.php
+++ coffeedrop-api/app/Models/Location.php
@@ -9,6 +9,16 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Location extends Model {
 	use HasFactory;
 
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array<int, string>
+	 */
+	protected $fillable = [
+		'postcode_id',
+		'week_id',
+	];
+
 	public function postcode(): BelongsTo {
 		return $this->belongsTo(Postcode::class);
 	}
