2205ca371080c5313980bd9aa5c5ee06a030750b
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-26 16:56:15 +0100

    Implement the `create_locations_table` migration

--- coffeedrop-api/database/migrations/2024_09_26_151609_create_locations_table.php
+++ coffeedrop-api/database/migrations/2024_09_26_151609_create_locations_table.php
@@ -1,27 +1,39 @@
 <?php
 
+use App\Models\Postcode;
+use App\Models\Week;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('locations', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void {
+		Schema::create('locations', function (Blueprint $table) {
+			$table->id();
+			$table->timestamps();
+			$table
+				->foreignIdFor(Postcode::class)
+				->unique()
+				->nullable(false)
+				->constrained()
+				->cascadeOnUpdate()
+				->cascadeOnDelete();
+			$table
+				->foreignIdFor(Week::class)
+				->nullable(false)
+				->constrained()
+				->cascadeOnUpdate()
+				->cascadeOnDelete();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('locations');
-    }
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void {
+		Schema::dropIfExists('locations');
+	}
 };
