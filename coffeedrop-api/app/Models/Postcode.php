7a127387b6ae3ea08b50f2523880735a82e7b07b
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-26 15:17:33 +0100

    Add `postcode()` mutator for `Postcode`

--- coffeedrop-api/app/Models/Postcode.php
+++ coffeedrop-api/app/Models/Postcode.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Clickbar\Magellan\Data\Geometries\Point;
 use Clickbar\Magellan\Database\Eloquent\HasPostgisColumns;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use JustSteveKing\LaravelPostcodes\Facades\Postcode as FacadesPostcode;
 
 class Postcode extends Model {
 	use HasFactory, HasPostgisColumns;
@@ -26,5 +29,20 @@ class Postcode extends Model {
 
 	public function location(): HasMany {
 		return $this->hasMany(Location::class);
+	}
+
+	protected function postcode(): Attribute {
+		return Attribute::make(
+			set: function ($value) {
+				$postcode = FacadesPostcode::getPostcode($value);
+				return [
+					'postcode' => $value,
+					'location' => Point::makeGeodetic(
+						latitude: $postcode->latitude,
+						longitude: $postcode->longitude
+					),
+				];
+			}
+		);
 	}
 }
