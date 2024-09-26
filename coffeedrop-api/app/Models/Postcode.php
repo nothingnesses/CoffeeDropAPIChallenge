fe37d561d83b82d55712d86986f7771e5a311a21
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-26 15:07:53 +0100

    php artisan magellan:update-postgis-columns

--- coffeedrop-api/app/Models/Postcode.php
+++ coffeedrop-api/app/Models/Postcode.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
+use Clickbar\Magellan\Database\Eloquent\HasPostgisColumns;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Postcode extends Model
 {
     use HasFactory;
+    use HasPostgisColumns;
+
+    protected array $postgisColumns = [
+        'location' => [
+            'type' => 'geography',
+            'srid' => 4326,
+        ],
+    ];
+
 }
