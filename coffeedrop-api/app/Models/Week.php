11833cda8c2f925e7931cea1ea62f8ce83ded1e5
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-27 11:15:32 +0100

    Define mass assignable attributes of `Week`

--- coffeedrop-api/app/Models/Week.php
+++ coffeedrop-api/app/Models/Week.php
@@ -11,6 +11,21 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class Week extends Model {
 	use HasFactory;
 
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array<int, string>
+	 */
+	protected $fillable = [
+		'day_' . Carbon::SUNDAY . '_id',
+		'day_' . Carbon::MONDAY . '_id',
+		'day_' . Carbon::TUESDAY . '_id',
+		'day_' . Carbon::WEDNESDAY . '_id',
+		'day_' . Carbon::THURSDAY . '_id',
+		'day_' . Carbon::FRIDAY . '_id',
+		'day_' . Carbon::SATURDAY . '_id',
+	];
+
 	public function location(): HasMany {
 		return $this->hasMany(Location::class);
 	}
