4c6bf2d73c2bdf7544c1e049265b53adceec0916
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-27 12:55:38 +0100

    Implement `WeekSeeder`

--- coffeedrop-api/database/seeders/WeekSeeder.php
+++ coffeedrop-api/database/seeders/WeekSeeder.php
@@ -2,16 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Imports\WeeksImport;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Maatwebsite\Excel\Facades\Excel;
 
-class WeekSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        //
-    }
+class WeekSeeder extends Seeder {
+	use WithoutModelEvents;
+
+	/**
+	 * Run the database seeds.
+	 */
+	public function run(): void {
+		Excel::import(new WeeksImport, public_path('location_data.csv'));
+	}
 }
