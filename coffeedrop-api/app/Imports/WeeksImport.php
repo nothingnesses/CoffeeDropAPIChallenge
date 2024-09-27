20a8577591798e41b192967122eb78547f742c1b
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-27 12:49:14 +0100

    Implement `WeeksImport`

--- coffeedrop-api/app/Imports/WeeksImport.php
+++ coffeedrop-api/app/Imports/WeeksImport.php
@@ -2,20 +2,70 @@
 
 namespace App\Imports;
 
+use App\Models\Day;
+use App\Models\Time;
 use App\Models\Week;
-use Maatwebsite\Excel\Concerns\ToModel;
+use Carbon\Carbon;
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\SkipsErrors;
+use Maatwebsite\Excel\Concerns\SkipsOnError;
+use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class WeeksImport implements ToModel
-{
-    /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
-    public function model(array $row)
-    {
-        return new Week([
-            //
-        ]);
-    }
+class WeeksImport implements ToCollection, WithHeadingRow, SkipsOnError {
+	use SkipsErrors;
+
+	/**
+	 * @param \Illuminate\Support\Collection $rows
+	 */
+	public function collection(Collection $rows) {
+		$get_time_id = fn($time) => Time::firstOrCreate(['time' => $time])->getKey();
+		$now = Carbon::now();
+		$days = collect([
+			Carbon::SUNDAY,
+			Carbon::MONDAY,
+			Carbon::TUESDAY,
+			Carbon::WEDNESDAY,
+			Carbon::THURSDAY,
+			Carbon::FRIDAY,
+			Carbon::SATURDAY,
+		])
+			->mapWithKeys(
+				function ($day) use ($now) {
+					$day_string = strtolower(
+						$now
+							->next($day)
+							->dayName
+					);
+					return [
+						$day => [
+							'open_time_key' => "open_{$day_string}",
+							'closed_time_key' => "closed_{$day_string}"
+						]
+					];
+				}
+			);
+		foreach ($rows as $row) {
+			Week::firstOrCreate(
+				$days
+					->mapWithKeys(
+						fn($keys, $day) => optional(
+							$row[$keys['open_time_key']] ?? null,
+							fn($open_time) => optional(
+								$row[$keys['closed_time_key']] ?? null,
+								fn($closed_time) => optional(
+									Day::firstOrCreate([
+										'day' => $day,
+										'open_time_id' => $get_time_id($open_time),
+										'closed_time_id' => $get_time_id($closed_time),
+									])->getKey(),
+									fn($day_model) => ["day_{$day}" => $day_model]
+								)
+							)
+						) ?? []
+					)
+					->all()
+			);
+		}
+	}
 }
