f60495adf690886f3579963f35a111737f569b7a
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-27 10:26:48 +0100

    Implement the `create_days_table` migration

--- coffeedrop-api/database/migrations/2024_09_25_152234_create_days_table.php
+++ coffeedrop-api/database/migrations/2024_09_25_152234_create_days_table.php
@@ -1,27 +1,57 @@
 <?php
 
+use App\Models\Time;
+use Carbon\Carbon;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('days', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void {
+		Schema::create('days', function (Blueprint $table) {
+			$table->id();
+			$table->timestamps();
+			$table
+				->enum(
+					'day',
+					[
+						Carbon::SUNDAY,
+						Carbon::MONDAY,
+						Carbon::TUESDAY,
+						Carbon::WEDNESDAY,
+						Carbon::THURSDAY,
+						Carbon::FRIDAY,
+						Carbon::SATURDAY,
+					]
+				)
+				->nullable(false);
+			$table
+				->foreignIdFor(Time::class, 'open_time_id')
+				->constrained('times')
+				->nullable(false)
+				->cascadeOnUpdate()
+				->cascadeOnDelete();
+			$table
+				->foreignIdFor(Time::class, 'closed_time_id')
+				->constrained('times')
+				->nullable(false)
+				->cascadeOnUpdate()
+				->cascadeOnDelete();
+			$table->unique([
+				'day',
+				'open_time_id',
+				'closed_time_id',
+			]);
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('days');
-    }
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void {
+		Schema::dropIfExists('days');
+	}
 };
