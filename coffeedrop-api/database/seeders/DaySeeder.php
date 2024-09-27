b11db58e154809530f060d2e32c46534a961c6e2
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-27 10:37:24 +0100

    Implement `DaySeeder`

--- coffeedrop-api/database/seeders/DaySeeder.php
+++ coffeedrop-api/database/seeders/DaySeeder.php
@@ -2,16 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Imports\DaysImport;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Maatwebsite\Excel\Facades\Excel;
 
-class DaySeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        //
-    }
+class DaySeeder extends Seeder {
+	use WithoutModelEvents;
+
+	/**
+	 * Run the database seeds.
+	 */
+	public function run(): void {
+		Excel::import(new DaysImport, public_path('location_data.csv'));
+	}
 }
