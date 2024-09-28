8f26feac60a469a0b90637658212cbab45ad7b98
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-28 08:01:37 +0100

    Add `index()` to `LocationController`

--- coffeedrop-api/app/Http/Controllers/LocationController.php
+++ coffeedrop-api/app/Http/Controllers/LocationController.php
@@ -2,9 +2,47 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\LocationRequest;
+use App\Http\Resources\LocationResource;
+use App\Models\Location;
+use App\Models\Postcode;
+use App\Services\LocationService;
+use Clickbar\Magellan\Database\PostgisFunctions\ST;
 
-class LocationController extends Controller
-{
-    //
+class LocationController extends Controller {
+	/**
+	 * Display a listing of the resource.
+	 */
+	public function index(LocationRequest $request) {
+		if ($request->has('nearest-to-postcode')) {
+			return LocationResource::make(
+				LocationService::denormalise(
+					Location::query()
+						->join(
+							'postcodes',
+							'locations.postcode_id',
+							'=',
+							'postcodes.id'
+						)
+						->stOrderBy(
+							ST::distanceSpheroid(
+								Postcode::firstOrCreate(
+									[
+										'postcode' => preg_replace(
+											'/\s+/',
+											'',
+											strtoupper($request->query('nearest-to-postcode'))
+										)
+									]
+								)->location,
+								'location'
+							)
+						)
+				)
+					->take(1)
+					->first()
+			);
+		}
+		return 'todo';
+	}
 }
