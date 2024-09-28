4234dcdb0bf71267f545f0c10aa9b5b968c27f86
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-28 08:01:37 +0100

    Add `index()` to `LocationController`

--- coffeedrop-api/app/Http/Controllers/LocationController.php
+++ coffeedrop-api/app/Http/Controllers/LocationController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\LocationRequest;
+use App\Http\Resources\LocationResource;
+use App\Models\Location;
 
-class LocationController extends Controller
-{
-    //
+class LocationController extends Controller {
+	/**
+	 * Display a listing of the resource.
+	 */
+	public function index(LocationRequest $request) {
+		if ($request->has('nearest-to-postcode')) {
+			return new LocationResource(
+				Location::scope_nearest_to_postcode($request->query('nearest-to-postcode'))
+					->scope_denormalise()
+					->first()
+			);
+		}
+		return 'todo';
+	}
 }
