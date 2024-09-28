b802e62eadbedaa6c81d758162de4c8438d31929
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-28 08:02:49 +0100

    Add "/locations/index" route to API

--- coffeedrop-api/routes/api.php
+++ coffeedrop-api/routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\LocationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
+	return $request->user();
 })->middleware('auth:api');
+
+Route::resource(
+	'locations',
+	LocationController::class,
+	['only' => ['index']]
+);
