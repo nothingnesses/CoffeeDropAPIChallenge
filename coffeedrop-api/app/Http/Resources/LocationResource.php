ef5850d6d78d12f7014f8338a1e52e99f58f2303
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-28 10:39:19 +0100

    Implement `LocationResource`

--- coffeedrop-api/app/Http/Resources/LocationResource.php
+++ coffeedrop-api/app/Http/Resources/LocationResource.php
@@ -2,18 +2,59 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class LocationResource extends JsonResource
-{
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
-    public function toArray(Request $request): array
-    {
-        return parent::toArray($request);
-    }
+class LocationResource extends JsonResource {
+	/**
+	 * Transform the resource into an array.
+	 *
+	 * @return array<string, mixed>
+	 */
+	public function toArray(Request $request): array {
+		$now = Carbon::now();
+		$format_time = fn($time) => Carbon::createFromTimeString($time)->format('H:i');
+		$result = [
+			'postcode' => $this
+				->postcode
+				->postcode
+		];
+		foreach (
+			[
+				Carbon::SUNDAY,
+				Carbon::MONDAY,
+				Carbon::TUESDAY,
+				Carbon::WEDNESDAY,
+				Carbon::THURSDAY,
+				Carbon::FRIDAY,
+				Carbon::SATURDAY
+			] as $day
+		) {
+			$day_name = strtolower(
+				$now
+					->next($day)
+					->dayName
+			);
+			if (!empty($this
+					->week
+					->$day_name)) {
+				$result['opening_times'][$day_name] = $format_time(
+					$this
+						->week
+						->$day_name
+						->opening_time
+						->time
+				);
+				$result['closing_times'][$day_name] = $format_time(
+					$this
+						->week
+						->$day_name
+						->closing_time
+						->time
+				);
+			}
+		}
+		return $result;
+	}
 }
