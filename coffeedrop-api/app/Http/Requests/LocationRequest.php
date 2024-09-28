4d25ed2aa62b8121dce1a080c1baff1a83af6c3d
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-28 07:59:37 +0100

    Add GET request rules for `LocationRequest`

--- coffeedrop-api/app/Http/Requests/LocationRequest.php
+++ coffeedrop-api/app/Http/Requests/LocationRequest.php
@@ -3,26 +3,45 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
-class LocationRequest extends FormRequest
-{
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
+class LocationRequest extends FormRequest {
+	/**
+	 * Determine if the user is authorized to make this request.
+	 */
+	public function authorize(): bool {
+		return true;
+	}
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
-    {
-        return [
-            //
-        ];
-    }
+	/**
+	 * Prepare the data for validation.
+	 */
+	protected function prepareForValidation(): void {
+		match ($this->method()) {
+			'GET' => optional(
+				$this->query('nearest-to-postcode'),
+				fn($postcode_parameter) => $this->merge([
+					'nearest-to-postcode' => $postcode_parameter,
+				])
+			),
+			default => null,
+		};
+	}
+
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+	 */
+	public function rules(): array {
+		return match ($this->method()) {
+			'GET' => [
+				'nearest-to-postcode' => [
+					'string',
+					Rule::postcode()
+				]
+			],
+			default => []
+		};
+	}
 }
