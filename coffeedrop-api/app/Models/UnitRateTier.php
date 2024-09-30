fd20736fe3433e4be92224ab79b73cd49f9a15c8
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-30 14:52:27 +0100

    Define `UnitRateTier` relationship methods

--- coffeedrop-api/app/Models/UnitRateTier.php
+++ coffeedrop-api/app/Models/UnitRateTier.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class UnitRateTier extends Model
-{
-    use HasFactory;
+class UnitRateTier extends Model {
+	use HasFactory;
+
+	public function as_tier_below(): HasMany {
+		return $this->hasMany(Self::class);
+	}
+
+	public function product(): HasMany {
+		return $this->hasMany(Product::class);
+	}
+
+	public function tier_below(): BelongsTo {
+		return $this->belongsTo(Self::class);
+	}
 }
