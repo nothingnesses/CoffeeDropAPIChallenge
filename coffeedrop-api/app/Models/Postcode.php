9bd89bd906a51113b278b5ddaa92fbcfdfd6c0f4
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-30 14:51:41 +0100

    Define `Postcode` relationship methods

--- coffeedrop-api/app/Models/Postcode.php
+++ coffeedrop-api/app/Models/Postcode.php
@@ -5,17 +5,19 @@ namespace App\Models;
 use Clickbar\Magellan\Database\Eloquent\HasPostgisColumns;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class Postcode extends Model
-{
-    use HasFactory;
-    use HasPostgisColumns;
+class Postcode extends Model {
+	use HasFactory, HasPostgisColumns;
 
-    protected array $postgisColumns = [
-        'location' => [
-            'type' => 'geography',
-            'srid' => 4326,
-        ],
-    ];
+	protected array $postgisColumns = [
+		'location' => [
+			'type' => 'geography',
+			'srid' => 4326,
+		],
+	];
 
+	public function location(): HasMany {
+		return $this->hasMany(Location::class);
+	}
 }
