73b1ee6c72566ec2d455be4e04151c89ba120d1a
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-30 10:07:17 +0100

    Implement the `create_cashback_product_table` migration

--- coffeedrop-api/database/migrations/2024_09_30_095414_create_cashback_product_table.php
+++ coffeedrop-api/database/migrations/2024_09_30_095414_create_cashback_product_table.php
@@ -1,27 +1,38 @@
 <?php
 
+use App\Models\Cashback;
+use App\Models\Product;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('cashback_product', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void {
+		Schema::create('cashback_product', function (Blueprint $table) {
+			$table->id();
+			$table->timestamps();
+			$table
+				->foreignIdFor(Cashback::class)
+				->nullable(false)
+				->constrained()
+				->cascadeOnUpdate()
+				->cascadeOnDelete();
+			$table
+				->foreignIdFor(Product::class)
+				->nullable(false)
+				->constrained()
+				->cascadeOnUpdate()
+				->cascadeOnDelete();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('cashback_product');
-    }
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void {
+		Schema::dropIfExists('cashback_product');
+	}
 };
