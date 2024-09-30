b0bb6d2dea6cf4e3f4f8f0897291260b36a340b2
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-30 14:50:12 +0100

    Define `Time` relationship methods

--- coffeedrop-api/app/Models/Time.php
+++ coffeedrop-api/app/Models/Time.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class Time extends Model
-{
-    use HasFactory;
+class Time extends Model {
+	use HasFactory;
+
+	public function as_opening_time(): HasMany {
+		return $this->hasMany(Day::class, 'opening_time_id');
+	}
+
+	public function as_closing_time(): HasMany {
+		return $this->hasMany(Day::class, 'closing_time_id');
+	}
 }
