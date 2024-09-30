0c8b18a29d8924f39fb00bda6dda4cd853684f65
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-30 13:18:33 +0100

    Add POST request rules for `CashbackRequest`

--- coffeedrop-api/app/Http/Requests/CashbackRequest.php
+++ coffeedrop-api/app/Http/Requests/CashbackRequest.php
@@ -2,27 +2,38 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Product;
+use Carbon\Carbon;
 use Illuminate\Foundation\Http\FormRequest;
 
-class CashbackRequest extends FormRequest
-{
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return false;
-    }
+class CashbackRequest extends FormRequest {
+	/**
+	 * Determine if the user is authorized to make this request.
+	 */
+	public function authorize(): bool {
+		return true;
+	}
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
-    {
-        return [
-            //
-        ];
-    }
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+	 */
+	public function rules(): array {
+		return match ($this->method()) {
+			'POST' => Product::pluck('name')
+				->map(
+					fn($product) => [
+						$product => [
+							'nullable',
+							'integer',
+							'numeric',
+							'gte:0'
+						]
+					]
+				)
+				->all(),
+			default => []
+		};
+	}
 }
