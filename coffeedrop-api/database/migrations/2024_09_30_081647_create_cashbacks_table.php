021f9ccef323ef5a1db35f213a34bb15d5f539e5
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-30 12:04:42 +0100

    Implement the `create_cashbacks_table` migration

--- coffeedrop-api/database/migrations/2024_09_30_081647_create_cashbacks_table.php
+++ coffeedrop-api/database/migrations/2024_09_30_081647_create_cashbacks_table.php
@@ -1,27 +1,34 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('cashbacks', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void {
+		Schema::create('cashbacks', function (Blueprint $table) {
+			$table->id();
+			$table->timestamps();
+			$table
+				->foreignIdFor(User::class)
+				->nullable(true)
+				->constrained()
+				->cascadeOnUpdate()
+				->cascadeOnDelete();
+			$table
+				->integer('total', unsigned: true)
+				->nullable(false);
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('cashbacks');
-    }
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void {
+		Schema::dropIfExists('cashbacks');
+	}
 };
