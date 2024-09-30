6799e8f0e80f62173d6c0391d77b34ad11e2fb57
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-30 09:00:19 +0100

    Implement the `create_products_table` migration

--- coffeedrop-api/database/migrations/2024_09_30_075432_create_products_table.php
+++ coffeedrop-api/database/migrations/2024_09_30_075432_create_products_table.php
@@ -1,27 +1,35 @@
 <?php
 
+use App\Models\UnitRateTier;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('products', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void {
+		Schema::create('products', function (Blueprint $table) {
+			$table->id();
+			$table->timestamps();
+			$table
+				->string('name')
+				->unique()
+				->nullable(false);
+			$table
+				->foreignIdFor(UnitRateTier::class)
+				->nullable(false)
+				->constrained()
+				->cascadeOnUpdate()
+				->cascadeOnDelete();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('products');
-    }
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void {
+		Schema::dropIfExists('products');
+	}
 };
