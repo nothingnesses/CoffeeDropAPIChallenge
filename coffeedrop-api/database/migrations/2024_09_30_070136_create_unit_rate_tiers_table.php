da84277da0a71e1311f14c0a7ffa6baaf0e5240e
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-30 08:23:08 +0100

    Implement the `create_unit_rate_tiers_table` migration

--- coffeedrop-api/database/migrations/2024_09_30_070136_create_unit_rate_tiers_table.php
+++ coffeedrop-api/database/migrations/2024_09_30_070136_create_unit_rate_tiers_table.php
@@ -1,27 +1,42 @@
 <?php
 
+use App\Models\UnitRateTier;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('unit_rate_tiers', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void {
+		Schema::create('unit_rate_tiers', function (Blueprint $table) {
+			$table->id();
+			$table->timestamps();
+			$table
+				->integer('minimum_amount', unsigned: true)
+				->nullable(false);
+			$table
+				->integer('rate', unsigned: true)
+				->nullable(false);
+			$table
+				->foreignIdFor(UnitRateTier::class, 'tier_below_id')
+				->nullable()
+				->constrained('unit_rate_tiers')
+				->cascadeOnUpdate()
+				->cascadeOnDelete();
+			$table->unique([
+				'minimum_amount',
+				'rate',
+				'tier_below_id'
+			]);
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('unit_rate_tiers');
-    }
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void {
+		Schema::dropIfExists('unit_rate_tiers');
+	}
 };
