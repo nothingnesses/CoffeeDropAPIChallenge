c1b1f9fdf86537b1808928a154817f6a5ff80b2a
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-30 14:53:25 +0100

    Define `Cashback` relationship methods

--- coffeedrop-api/app/Models/Cashback.php
+++ coffeedrop-api/app/Models/Cashback.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-class Cashback extends Model
-{
-    use HasFactory;
+class Cashback extends Model {
+	use HasFactory;
+
+	public function products(): BelongsToMany {
+		return $this
+			->belongsToMany(Product::class)
+			->withPivot('amount');
+	}
+
+	public function user(): BelongsTo {
+		return $this->belongsTo(User::class);
+	}
 }
