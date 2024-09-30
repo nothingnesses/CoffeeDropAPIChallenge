ebbbdcc8434502fede221bafbb7bbc4e51c176f9
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-30 20:17:52 +0100

    Add `store()` to `CashbackController`

--- coffeedrop-api/app/Http/Controllers/CashbackController.php
+++ coffeedrop-api/app/Http/Controllers/CashbackController.php
@@ -2,9 +2,59 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\CashbackRequest;
+use App\Http\Resources\CashbackResource;
+use App\Models\Cashback;
+use App\Models\Product;
+use App\Models\User;
 
-class CashbackController extends Controller
-{
-    //
+class CashbackController extends Controller {
+	/**
+	 * Store a newly created resource in storage.
+	 */
+	public function store(CashbackRequest $request) {
+		$total = 0;
+		foreach ($request->collect() as $product => $amount) {
+			$amount_remaining = $amount;
+			foreach (
+				Product::firstWhere('name', $product)
+					->unit_rate_tiers
+					->sortByDesc('minimum_amount') as $current_tier
+			) {
+				if ($amount_remaining <= 0)
+					break;
+				if ($amount_remaining >= $current_tier->minimum_amount) {
+					$amount_this_tier = $amount_remaining - $current_tier->minimum_amount;
+					$total += $amount_this_tier * $current_tier->rate;
+					$amount_remaining -= $amount_this_tier;
+				}
+			}
+		}
+		$cashback = Cashback::create([
+			'user_id' => optional(
+				auth('api')->user(),
+				fn(User $user) => $user->getKey()
+			),
+			'total' => $total
+		]);
+		$cashback
+			->products()
+			->sync(
+				Product::whereIn(
+					'name',
+					$request
+						->collect()
+						->keys()
+				)
+					->get()
+					->mapWithKeys(
+						fn($product) => [
+							$product->getKey() => [
+								'amount' => $request->input($product->name)
+							]
+						]
+					)
+			);
+		return CashbackResource::make($cashback);
+	}
 }
