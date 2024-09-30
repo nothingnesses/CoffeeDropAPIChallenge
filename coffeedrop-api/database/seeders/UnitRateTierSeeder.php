1720df8dc0b30a9f7dbc913c476600ef5a7f4e16
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-30 08:43:28 +0100

    Implement `UnitRateTierSeeder`

--- coffeedrop-api/database/seeders/UnitRateTierSeeder.php
+++ coffeedrop-api/database/seeders/UnitRateTierSeeder.php
@@ -2,16 +2,36 @@
 
 namespace Database\Seeders;
 
+use App\Models\UnitRateTier;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class UnitRateTierSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        //
-    }
+class UnitRateTierSeeder extends Seeder {
+	use WithoutModelEvents;
+
+	/**
+	 * Run the database seeds.
+	 */
+	public function run(): void {
+		foreach (
+			[
+				[2, 3, 5],
+				[4, 6, 10],
+				[6, 9, 15]
+			] as [$first, $second, $third]
+		) {
+			UnitRateTier::firstOrCreate([
+				'minimum_amount' => 500,
+				'rate' => $third
+			]);
+			UnitRateTier::firstOrCreate([
+				'minimum_amount' => 50,
+				'rate' => $second
+			]);
+			UnitRateTier::firstOrCreate([
+				'minimum_amount' => 0,
+				'rate' => $first
+			]);
+		}
+	}
 }
