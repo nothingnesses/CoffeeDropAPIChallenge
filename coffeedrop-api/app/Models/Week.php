513dc9400f11f84d2b788cd56224efab19465a35
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-30 14:51:02 +0100

    Define `Week` relationship methods

--- coffeedrop-api/app/Models/Week.php
+++ coffeedrop-api/app/Models/Week.php
@@ -2,10 +2,44 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class Week extends Model
-{
-    use HasFactory;
+class Week extends Model {
+	use HasFactory;
+
+	public function location(): HasMany {
+		return $this->hasMany(Location::class);
+	}
+
+	public function sunday(): BelongsTo {
+		return $this->belongsTo(Day::class, 'day_' . Carbon::SUNDAY . '_id');
+	}
+
+	public function monday(): BelongsTo {
+		return $this->belongsTo(Day::class, 'day_' . Carbon::MONDAY . '_id');
+	}
+
+	public function tuesday(): BelongsTo {
+		return $this->belongsTo(Day::class, 'day_' . Carbon::TUESDAY . '_id');
+	}
+
+	public function wednesday(): BelongsTo {
+		return $this->belongsTo(Day::class, 'day_' . Carbon::WEDNESDAY . '_id');
+	}
+
+	public function thursday(): BelongsTo {
+		return $this->belongsTo(Day::class, 'day_' . Carbon::THURSDAY . '_id');
+	}
+
+	public function friday(): BelongsTo {
+		return $this->belongsTo(Day::class, 'day_' . Carbon::FRIDAY . '_id');
+	}
+
+	public function saturday(): BelongsTo {
+		return $this->belongsTo(Day::class, 'day_' . Carbon::SATURDAY . '_id');
+	}
 }
