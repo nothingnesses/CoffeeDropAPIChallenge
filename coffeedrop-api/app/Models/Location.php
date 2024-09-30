39cb8324c257cbb26e6459eacbae9bd46e4e8a0a
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-30 14:52:05 +0100

    Define `Location` relationship methods

--- coffeedrop-api/app/Models/Location.php
+++ coffeedrop-api/app/Models/Location.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Location extends Model
-{
-    use HasFactory;
+class Location extends Model {
+	use HasFactory;
+
+	public function postcode(): BelongsTo {
+		return $this->belongsTo(Postcode::class);
+	}
+
+	public function week(): BelongsTo {
+		return $this->belongsTo(Week::class);
+	}
 }
