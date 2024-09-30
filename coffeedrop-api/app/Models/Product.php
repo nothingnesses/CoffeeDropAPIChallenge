45927dc1ec3d2e80c88c63a1dfce0731dc78764c
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-09-30 14:53:01 +0100

    Define `Product` relationship methods

--- coffeedrop-api/app/Models/Product.php
+++ coffeedrop-api/app/Models/Product.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-class Product extends Model
-{
-    use HasFactory;
+class Product extends Model {
+	use HasFactory;
+
+	public function cashbacks(): BelongsToMany {
+		return $this->belongsToMany(Cashback::class);
+	}
+
+	public function unit_rate_tier(): BelongsTo {
+		return $this->belongsTo(UnitRateTier::class);
+	}
 }
