a009293eb8ccee59bb83035427339dc595ecf72b
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-10-01 16:01:27 +0100

    Register alias for `CheckClientCredentials` middleware

--- coffeedrop-api/bootstrap/app.php
+++ coffeedrop-api/bootstrap/app.php
@@ -3,17 +3,21 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Laravel\Passport\Http\Middleware\CheckClientCredentials;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-    )
-    ->withMiddleware(function (Middleware $middleware) {
-        //
-    })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+	->withRouting(
+		web: __DIR__ . '/../routes/web.php',
+		api: __DIR__ . '/../routes/api.php',
+		commands: __DIR__ . '/../routes/console.php',
+		health: '/up',
+	)
+	->withMiddleware(function (Middleware $middleware) {
+		$middleware->alias([
+			'client' => CheckClientCredentials::class
+		]);
+	})
+	->withExceptions(function (Exceptions $exceptions) {
+		//
+	})
+	->create();
