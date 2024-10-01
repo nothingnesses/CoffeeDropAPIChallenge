280615da5b15786131e2ec0f75c7087be583eada
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-10-01 09:53:07 +0100

    Implement `CashbackResource`

--- coffeedrop-api/app/Http/Resources/CashbackResource.php
+++ coffeedrop-api/app/Http/Resources/CashbackResource.php
@@ -2,18 +2,20 @@
 
 namespace App\Http\Resources;
 
+use App\Custom\Helpers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class CashbackResource extends JsonResource
-{
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
-    public function toArray(Request $request): array
-    {
-        return parent::toArray($request);
-    }
+class CashbackResource extends JsonResource {
+	/**
+	 * Transform the resource into an array.
+	 *
+	 * @return array<string, mixed>
+	 */
+	public function toArray(Request $request): array {
+		return match ($request->method()) {
+			'POST' => Helpers::pence_to_pounds_and_pence($this->total),
+			default => [],
+		};
+	}
 }
