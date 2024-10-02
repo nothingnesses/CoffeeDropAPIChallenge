cfc375e5e6429c5832562e31b8bbb9d7f3faeeb0
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-10-02 11:38:45 +0100

    Implement `create_product_unit_rate_tier_table` migration

--- coffeedrop-api/database/migrations/2024_09_30_075433_create_product_unit_rate_tier_table.php
+++ coffeedrop-api/database/migrations/2024_09_30_075433_create_product_unit_rate_tier_table.php
@@ -1,27 +1,42 @@
 <?php
 
+use App\Models\Product;
+use App\Models\UnitRateTier;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('product_unit_rate_tier', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 */
+	public function up(): void {
+		Schema::create('product_unit_rate_tier', function (Blueprint $table) {
+			$table->id();
+			$table->timestamps();
+			$table
+				->foreignIdFor(Product::class)
+				->nullable(false)
+				->constrained()
+				->cascadeOnUpdate()
+				->cascadeOnDelete();
+			$table
+				->foreignIdFor(UnitRateTier::class)
+				->nullable(false)
+				->constrained()
+				->cascadeOnUpdate()
+				->cascadeOnDelete();
+			$table->unique([
+				'product_id',
+				'unit_rate_tier_id'
+			]);
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('product_unit_rate_tier');
-    }
+	/**
+	 * Reverse the migrations.
+	 */
+	public function down(): void {
+		Schema::dropIfExists('product_unit_rate_tier');
+	}
 };
