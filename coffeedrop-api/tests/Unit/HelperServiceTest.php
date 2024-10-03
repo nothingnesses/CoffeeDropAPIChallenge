81b976ba7cb3d97a66a57f5cc19b140be23cf2fe
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-10-03 15:29:23 +0100

    Add `test_inverse_division_with_remainder`

--- coffeedrop-api/tests/Unit/HelperServiceTest.php
+++ coffeedrop-api/tests/Unit/HelperServiceTest.php
@@ -2,15 +2,27 @@
 
 namespace Tests\Unit;
 
+use App\Services\HelperService;
 use PHPUnit\Framework\TestCase;
 
-class HelperServiceTest extends TestCase
-{
-    /**
-     * A basic unit test example.
-     */
-    public function test_example(): void
-    {
-        $this->assertTrue(true);
-    }
+class HelperServiceTest extends TestCase {
+	public function test_inverse_division_with_remainder(): void {
+		$inverse = fn($divisor, $quotient, $remainder) => $quotient * $divisor + $remainder;
+		foreach (
+			collect()
+				->range(0, 999)
+				->map(
+					function () {
+						while (true) {
+							$result = (lcg_value() / 100) * 100;
+							if ($result > 0) {
+								return $result;
+							}
+						}
+					}
+				) as $item
+		) {
+			$this->assertTrue($inverse(100, ...HelperService::division_with_remainder($item, 100)) === $item, $item);
+		}
+	}
 }
