88950089e6453755f8582f3d0dac98f1c20fb060
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2024-10-03 10:58:18 +0100

    Add "/tokens/index" route to API

--- coffeedrop-api/routes/api.php
+++ coffeedrop-api/routes/api.php
@@ -3,6 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/tokens/index', function (Request $request) {
+	return \App\Models\User::find($request->query('user_id'))
+		->createToken($request->query('name'))
+		->accessToken;
+});
+
 Route::get('/user', function (Request $request) {
-    return $request->user();
+	return $request->user();
 })->middleware('auth:api');
